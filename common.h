fb7e04500ab620e12f3a9c99e16f561988bf340d
Serpent03 <dumbpilot03@gmail.com>
2024-03-02 10:38:05 +0530

    feat: init commit

---
+++ common.h
@@ -0,0 +1,20 @@
+#pragma once 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef u_int8_t u8;
+typedef int8_t i8;
+
+typedef u_int16_t u16;
+typedef int16_t i16;
+
+typedef u_int32_t u32;
+typedef int32_t i32;
+
+// REF: http://www.c-jump.com/CIS24/Slides/FAT/lecture.html
+
+
+
+
---
+++ fat.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "common.h"
+
+// REF: http://www.c-jump.com/CIS24/Slides/FAT/lecture.html
+
+#define DRIVE "drive.bin"
+
+typedef struct FAT {
+  // boot sector
+  // root dir
+  // data cluster (  )
+} FAT;
+
+
+typedef struct root_dir {} root_dir;
+
+typedef struct cluster {} cluster;
+
+typedef struct sector {} sector;
+
+
+
+
